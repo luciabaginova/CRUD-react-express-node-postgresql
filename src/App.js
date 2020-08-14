70087017089bfa8d65f323393b63779ddc11af58
Jameson Campbell <jcampbell@flexterapp.com>
2020-08-14 12:24:47 -0500

    grid styling

--- src/App.js
+++ src/App.js
@@ -24,28 +24,43 @@ const App = () => {
       <h1>React, Express, Node, Postgresql</h1>
       <h5>A simple app to create, read, update and delete data</h5>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(item =>
-            <tr key={item.id}>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
-              <td>
-                <button className="button" >Edit</button>
-                <button style={{ marginLeft: 5 }} className="button" >Delete</button>
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <div className="flex-row">
+      
+        <div className="flex-large">
+          <form>
+            <label>Name</label>
+            <input type="text" id="name" placeholder="First Name" />
+            <label>Email</label>
+            <input type="text" id="email" placeholder="Email" />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
 
+        <div className="flex-large">
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map(item =>
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>
+                    <button className="button" >Edit</button>
+                    <button style={{ marginLeft: 5 }} className="button" >Delete</button>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+
+      </div>
     </div>
   );
 }
