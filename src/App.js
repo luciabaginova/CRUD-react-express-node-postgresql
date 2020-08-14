4be25a9b973e2278c05903eaa1c0d2c7ea5a2e9c
Jameson Campbell <jcampbell@flexterapp.com>
2020-08-14 14:17:14 -0500

    edit user data function and ternary

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const App = () => {
 
   const [users, setUsers] = useState([])
   const [currentUser, setCurrentUser] = useState(initialFormState)
+  const [editing, setEditing] = useState(false)
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -39,8 +40,8 @@ const App = () => {
       },
       body: JSON.stringify(currentUser),
     })
-    .then(response => console.log(response))
-    
+      .then(response => console.log(response))
+
     setCurrentUser(initialFormState)
   }
 
@@ -53,9 +54,30 @@ const App = () => {
       },
     })
     response
-    .json()
-    .then(result => setUsers(result))
-    .catch(e => console.log(e))
+      .json()
+      .then(result => setUsers(result))
+      .catch(e => console.log(e))
+  }
+
+  const editUser = item => {
+    console.log(item)
+    setEditing(true)
+    setCurrentUser({id: item.id, name: item.name, email: item.email})
+  }
+
+  const submitUserEdit = event => {
+    event.preventDefault()
+
+    fetch(`http://localhost:8080/users/${currentUser.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(currentUser),
+    })
+      .then(response => console.log(response))
+
+    setCurrentUser(initialFormState)
   }
 
 
@@ -66,27 +88,51 @@ const App = () => {
 
       <div className="flex-row">
 
-        <div className="flex-large">
-          <form onSubmit={submitNewUser}>
-            <label>Name</label>
-            <input
-              type="text"
-              id="name"
-              placeholder="Jane Doe"
-              onChange={handleInputChange}
-              value={currentUser.name}
-            />
-            <label>Email</label>
-            <input
-              type="text"
-              id="email"
-              placeholder="jane.doe@gmail.com"
-              onChange={handleInputChange}
-              value={currentUser.email}
-            />
-            <input type="submit" value="Submit" />
-          </form>
-        </div>
+        {editing ?
+          <div className="flex-large">
+            <form onSubmit={submitUserEdit}>
+              <label>Name</label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Jane Doe"
+                onChange={handleInputChange}
+                value={currentUser.name}
+              />
+              <label>Email</label>
+              <input
+                type="text"
+                id="email"
+                placeholder="jane.doe@gmail.com"
+                onChange={handleInputChange}
+                value={currentUser.email}
+              />
+              <input type="submit" value="Edit" />
+            </form>
+          </div>
+          :
+          <div className="flex-large">
+            <form onSubmit={submitNewUser}>
+              <label>Name</label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Jane Doe"
+                onChange={handleInputChange}
+                value={currentUser.name}
+              />
+              <label>Email</label>
+              <input
+                type="text"
+                id="email"
+                placeholder="jane.doe@gmail.com"
+                onChange={handleInputChange}
+                value={currentUser.email}
+              />
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+        }
 
         <div className="flex-large">
           <table>
@@ -103,7 +149,7 @@ const App = () => {
                   <td>{item.name}</td>
                   <td>{item.email}</td>
                   <td>
-                    <button className="button" >Edit</button>
+                    <button onClick={() => editUser(item)} className="button" >Edit</button>
                     <button onClick={() => deleteUser(item)} style={{ marginLeft: 5 }} className="button" >Delete</button>
                   </td>
                 </tr>
