647516b0b01dda64e77b2ea46c3815ae6a2c11a5
Jameson Campbell <jcampbell@flexterapp.com>
2020-08-14 11:55:32 -0500

    created table

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ const App = () => {
     <div className="container">
       <h1>React, Express, Node, Postgresql</h1>
       <p1>A simple app to create, react, update and delete data</p1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jane Doe</td>
+            <td>janedoe@gmail.com</td>
+            <td>
+              <button className="button" >Edit</button>
+              <button style={{ marginLeft: 5 }} className="button" >Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
     </div>
   );
 }
